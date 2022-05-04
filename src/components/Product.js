fb2af7f14c5c58d51219dedcf2845ae1db04c56e
Artem Adams <artemadams@gmail.com>
2022-05-04 13:24:51 -0400

    click counter

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProductDescription from "./ProductDescription";
 
 import { ShoppingCartIcon } from "@heroicons/react/outline";
